263935dc834cc30769e84b8a311b759b9d1df875
Priyank3117 <priyank.patel@etatvasoft.com>
2024-02-22 16:18:56 +0530

    ...

--- HelloDoc/Controllers/Admin_DashController.cs
+++ HelloDoc/Controllers/Admin_DashController.cs
@@ -57,6 +57,31 @@ namespace HelloDoc.Controllers
 
             return PartialView(partialName,DashData);
 
-        }
+        } 
+        
+        
+        public IActionResult SearchPatient(string SearchValue,string Filterselect)
+               {
+            var FilterData = (from req in _context.Requests
+                            join reqclient in _context.RequestClients
+                            on req.RequestId equals reqclient.RequestId
+
+                            select new Admin_DashBoard()
+                            {
+                                Name = reqclient.FirstName.ToLower(),
+                                Requestor = req.FirstName,
+                                BirthDate = (new DateTime((int)reqclient.IntYear, int.Parse(reqclient.StrMonth), (int)reqclient.IntDate)).ToString("MMM dd,yyyy"),
+                                RequestedDate = req.CreatedDate,
+                                PhoneNumber = req.PhoneNumber,
+                                PhoneNumber_P = reqclient.PhoneNumber,
+                                requesttypeid = req.RequestTypeId,
+                                Address = reqclient.Street + " " + reqclient.City
+
+                            }).Where(item => (string.IsNullOrEmpty(SearchValue) || item.Name.Contains(SearchValue)) && (string.IsNullOrEmpty(Filterselect) || item.requesttypeid == int.Parse(Filterselect) )).ToList();
+
+
+            return PartialView("Admin_Table", FilterData);
+
+            }
     }
 }
